cb58c04a1817de877ce777fa9a3dfcde98abf4de
Laby_ <fingskim0229@gmail.com>
2023-06-21 14:34:13 +0900

    WIP #2

--- src/follower.py
+++ src/follower.py
@@ -6,6 +6,11 @@ from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
 
 FOLLOWER_NUM = 2
+HEADING = 180        # 
+DETECTION_ANGLE = 30    # The angle of detection. Total range will be double of this
+DISTANCE = 0.4          # Target distance
+MARGIN = 0.05           # DISTANCE +- MARGIN will be the detection range
+SPEED = 0.1             # Default movement speed
 
 class Follower():
     def __init__(self, index):
@@ -14,7 +19,7 @@ class Follower():
         self.vel_msg = Twist()
 
     def scan_callback(self, data):
-        self.scan_data = data
+        self.scan_data = data.ranges
 
     def object_detection(self):
         pass
@@ -36,5 +41,5 @@ if __name__ == '__main__':
 
     r = rospy.Rate(5)
     while not rospy.is_shutdown():
-        # follower_essential.update()
+        # follower.update()
         r.sleep()
