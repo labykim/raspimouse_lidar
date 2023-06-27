e6ed232ee6c3b13f931c94d9ed9171f8a04dfc90
Laby_ <fingskim0229@gmail.com>
2023-06-27 12:31:14 +0900

    Boids WIP #2

--- src/boids.py
+++ src/boids.py
@@ -13,16 +13,16 @@ class Boid:
         self.vel_pub = rospy.Publisher('/raspi_' + str(index) + '/cmd_vel', Twist, queue_size=1)
         self.vel_msg = Twist()
 
+        # Store the angle and range information of other boids
+        # boid_pos[0] is always the leader
+        self.boid_pos = [[] for _ in range(BOID_NUM)]
+
         rospy.loginfo("Subscriber & publisher for /raspi_" + str(index) + "established.")
 
     # scan_callback function for the first time
     # Include initialization
     def scan_callback_init(self, data):
         self.data_new = data.ranges
-
-        # Store the angle and range information of other boids
-        # boid_pos[0] is always the leader
-        self.boid_pos = [[] for _ in range(BOID_NUM)]
 
     def scan_callback(self, data):
         self.data_old = self.data_new
@@ -50,23 +50,38 @@ class Boid:
         
         self.vel_pub.publish(self.vel_msg)
 
+    def condition_check(self, index):
+        if self.boid_pos[0] == None:
+            rospy.loginfo("Leader is not detected. Place the leader boid in front of raspi_" + str(index))
+
+
+
 if __name__ == '__main__':
     rospy.init_node('boids_master')
     rospy.loginfo('Boids master node initialized.')
 
     # Instantiate [BOID_NUM] Boid classes. Each class will be in charge of one raspimouse.
+    # LEADER IS NOT IN THIS ARRAY
     boids = [Boid(i) for i in range(BOID_NUM)]
 
+    # Loop for condition check before starting Boids
     r = rospy.Rate(1)
-    isReady = -1
     while not rospy.is_shutdown():
-        if isReady == 1:
-            r.sleep()
-            continue
-
-        for i in range(1, BOID_NUM):
-            isReady = 1
-            if boids[i].boid_pos[0] == None:
-                rospy.loginfo("Leader is not detected. Place the leader boid in front of raspi_" + str(i))
+        isReady = 1
+        for i in range(BOID_NUM):
+            if boids[i].condition_check(i) == False:
                 isReady = 0
                 break
+        if isReady:
+            break
+        r.sleep()
+
+    rospy.loginfo("Initial condition satisfied. Starting Boids...")
+
+    # Loop for live update
+    # Nothing yet since the main algorithm is run by scan_callback
+    # TODO: Maybe some superviser thing?
+    r = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        # something
+        r.sleep()
